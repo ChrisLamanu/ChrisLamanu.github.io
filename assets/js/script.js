2523906cb7e9068875a7e4cda91c631aff7714d6
ChrisLamanu <christopher.vrel@gmail.com>
2020-12-01 23:00:45 +0100

    "some fix"

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,6 @@ let qrcode = document.getElementById('qrcode');
 let pageAcceuil = document.getElementById('pageAcceuil');
 let containerPageJeu = document.getElementById('containerPageJeu');
 
-qrcode.addEventListener('click', function(){
-    pageAcceuil.classList.add('d-none');
-    containerPageJeu.classList.add('d-block');
-})
-
 let interval = 1000;
 
 let sec = 00;
@@ -23,20 +18,35 @@ let hour = 00;
 
 let verifTimer = false;
 
-/* circleSizeArray.forEach(element => {
-    element.addEventListener('click', function(){
-        console.log(this)
-    })
-}); */
+let imgHead = document.querySelectorAll(".imgHead");
 
+let headArray = [];
+    headArray[0] = "assets/img/Anousone-Mounivongs-400.png";
+    headArray[1] = "assets/img/frederik-noel-formateur-la-manu-formation.png";
+    headArray[2] = "assets/img/Nicolas-Vallois-400.png";
+
+let headOnScreenCount = 0;
+let headMax = 9;
+
+let soundA = document.getElementById("soundA");
+let soundB = document.getElementById("soundB");
+var score = 0;
+var difficulty = 0;
+
+qrcode.addEventListener('click', function(){
+    pageAcceuil.setAttribute("style", "display: none!important;");
+    containerPageJeu.setAttribute("style", "display: block!important;");
+})
 
 startGame.addEventListener('click', function(){
-    verifTimer = true;
-    activateClock();
+    if (!verifTimer) {
+        verifTimer = true;
+        activateClock();
+    }
 })
 
 function activateClock(){
-    if( verifTimer == true){
+    if  (verifTimer == true){
         setInterval(clock, 1000);
     } else {
         setInterval(clock, 0);
@@ -49,73 +59,26 @@ function activateClock(){
 }
 
 function clock(){
-    /*var message = document.getElementById("timer");
-    message.innerHTML = "The time is " + Date();
-    */
-
     sec += 1;
-    console.log(hour + ":" + min + ":" + sec);
+
     if (sec == 60) {
         min += 1;
         sec = 00;
-        console.log(hour + ":" + min + ":" + sec);
+
         if (min == 60) {
             min = 00;
             hour += 1;
-            console.log(hour + ":" + min + ":" + sec);
         }
     }
+
     document.getElementById("timerJeu").innerHTML = hour + ":" + min + ":" + sec;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let imgHead = document.querySelectorAll(".imgHead");
-
-let headArray = [];
-    headArray[0] = "assets/img/Anousone-Mounivongs-400.png";
-    headArray[1] = "assets/img/frederik-noel-formateur-la-manu-formation.png";
-    headArray[2] = "assets/img/Nicolas-Vallois-400.png";
-    // After launch random img appear with delay
-
-let headOnScreenCount = 0;
-let headMax = 9;
-
-
-
 function addHead() {
-
-
     let rIndex = Math.floor(Math.random() * headArray.length);
     let imgRIndex = Math.floor(Math.random() * imgHead.length);
     
-    if (imgHead[imgRIndex].getAttribute("src") == "") {
-        console.log("img src empty");
-        
-        
+    if (imgHead[imgRIndex].getAttribute("src") == "assets/img/invisible.png") {
         imgHead[imgRIndex].style.transition = "opacity .3s ease-in";
         imgHead[imgRIndex].style.opacity = "1";
         imgHead[imgRIndex].src = headArray[rIndex];
@@ -128,36 +91,26 @@ function addHead() {
 
 function deleteHead(a) {
     window.setTimeout( () => { 
-        
-
         a.style.opacity = "0";
         
         headOnScreenCount--;
     }, interval);
 }
 
-let soundA = document.getElementById("soundA");
-let soundB = document.getElementById("soundB");
-var score = 0;
-var difficulty = 0;
 imgHead.forEach(e => {
     e.addEventListener("click", () => {
         let r = Math.floor(Math.random() * 2);
         console.log(r);
-        if (e.getAttribute("src") != "") {
+        if (e.getAttribute("src") != "assets/img/invisible.png") {
             if (r == 1) {
                 soundA.play();
             }
             else {
                 soundB.play();
             }
-            e.src = "";
+            e.src = "assets/img/invisible.png";
                
             headOnScreenCount--;
-
-
-
-
 
             if (difficulty < 10){
                 score += 10;
@@ -179,47 +132,6 @@ imgHead.forEach(e => {
             }
             difficulty++;
             $("#score").html(score);
-            console.log(difficulty)
-            console.log(score)
         }
-
-        // if (imgHead[imgRIndex].getAttribute("src") != "") {
-        //     console.log("eeeeeee " + e.src)
-        //     e.src = "";
-            
-        //     headOnScreenCount--;
-        // }
     });
 });
-// $(document).ready(function(){
-//     var score = 0;
-//     var difficulty = 0;
-//     $(".imgHead").click(function(e){ 
-//         if (e.attr("src") != "") {
-//             if (difficulty < 10){
-//                 score += 10;
-//             }
-//             else if (difficulty >= 10){
-//                 score += 20;
-//                 interval = 800;
-//             }
-//             else if (difficulty >= 35){
-//                 score += 60;
-//                 interval = 700;
-//             }
-//             else if (difficulty >= 50){
-//                 score += 100;
-//                 interval = 650;
-//             }
-//             if (score > 1000) {
-//                 interval = 300;
-//             }
-//             difficulty++;
-//             $("#score").html(score);
-//             console.log(difficulty)
-//             console.log(score)
-//         }
-
-//     });
-// });
-
